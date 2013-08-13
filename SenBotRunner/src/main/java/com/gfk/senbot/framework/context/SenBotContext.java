0c89e09bfd644ae970d2d1c7aaad593ba41b225e
Joost Schouten <joost@yanomo.com>
2013-08-13 14:55:39 +0200

    Changed the spring bean def file name

--- SenBotRunner/src/main/java/com/gfk/senbot/framework/context/SenBotContext.java
+++ SenBotRunner/src/main/java/com/gfk/senbot/framework/context/SenBotContext.java
@@ -102,7 +102,7 @@ public class SenBotContext {
                 if (senBotContextSingleton == null) {
                 	String springConfigLocation = System.getProperty(SPRING_CONFIG_LOCATION);
                 	if(springConfigLocation == null) {
-                		springConfigLocation = "cucumber.xml";
+                		springConfigLocation = "spring/senbot-runner-beans.xml";
                 	}
                 	
                     context = new ClassPathXmlApplicationContext(new String[]{springConfigLocation});
