63626539b08094c1ecc4846da825fb622c6cf517
Joost Schouten <joost@yanomo.com>
2014-01-23 11:04:13 +0100

    Start of integrated step decumentation

---
+++ SenBotDocumenter/src/main/java/com/gfk/senbot/documenter/CucumberDoclet.java
@@ -0,0 +1,15 @@
+package com.gfk.senbot.documenter;
+
+import com.sun.javadoc.RootDoc;
+import com.sun.tools.doclets.formats.html.HtmlDoclet;
+import com.sun.tools.doclets.internal.toolkit.AbstractDoclet;
+
+public class CucumberDoclet extends HtmlDoclet {
+
+	@Override
+	public boolean start(AbstractDoclet doclet, RootDoc root) {
+		return super.start(doclet, root);
+	}
+	
+	
+}
